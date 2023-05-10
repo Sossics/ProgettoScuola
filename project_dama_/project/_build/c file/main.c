81757939a76f097d97f4ed81d09878f184e51f90
SGBORTO <filippobortoluzzi@gmail.com>
2023-05-10 12:21:26 +0200

    aggiornamento file
    y

--- project_dama_/project/_build/c file/main.c
+++ project_dama_/project/_build/c file/main.c
@@ -1,128 +1,162 @@
 #include "raylib.h"
 #include "raymath.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <malloc.h>
-#include <string.h>
 
-#define SQUARE_SIZE 80
 #define BOARD_SIZE 8
-
-typedef int BOARD;
-typedef Texture2D OBJ_DAMA;
-typedef int POSITION_X;
-typedef int POSITION_Y;
+#define SQUARE_SIZE 64
 
 typedef struct {
-    POSITION_X x;
-    POSITION_Y y;
-    OBJ_DAMA texture;
-}white;
-white* PAWN_WHITE[12];
+    struct board {
+        int mat[BOARD_SIZE][BOARD_SIZE];
+        int x;
+        int y;
+        Texture2D white_place;
+        Texture2D black_place;
+    } board;
+    struct white_pawn_ {
+        int mat[BOARD_SIZE][BOARD_SIZE];
+        int x;
+        int y;
+        Texture2D white_pawn;
+    } white_pawn_;
+    struct black_pawn_ {
+        int mat[BOARD_SIZE][BOARD_SIZE];
+        int x;
+        int y;
+        Texture2D black_pawn;
+    } black_pawn_;
+    struct game{
+        bool game_over;
+        bool game_run;
+        bool game_1v1;
+        bool game_bot;
+        bool Game_menu;
+    };
+} game;
 
 typedef struct {
-    POSITION_X x;
-    POSITION_Y y;
-    OBJ_DAMA texture;
-}nigga;
-nigga* PAWN_BLACK[12];
+    Rectangle rect;
+    const char* text;
+    int fontSize;
+    Color color;
+    Color textColor;
+    bool clicked;
+} Button;
 
-typedef struct {
-    BOARD mat[BOARD_SIZE][BOARD_SIZE];
-    OBJ_DAMA Black_space;
-    OBJ_DAMA White_space;
-}BOARD_P;
+// Funzione per disegnare un bottone
+void DrawButton(Button button) {
+    DrawRectangleRec(button.rect, button.color);
+    DrawText(button.text, button.rect.x + button.fontSize / 2, button.rect.y + button.fontSize / 2, button.fontSize, button.textColor);
+}
 
-BOARD_P BOARDP;
+// Funzione per creare un bottone
+Button CreateButton(float x, float y, float width, float height, const char* text, int fontSize, Color color, Color textColor) {
+    Button button;
+    button.rect.x = x;
+    button.rect.y = y;
+    button.rect.width = width;
+    button.rect.height = height;
+    button.text = text;
+    button.fontSize = fontSize;
+    button.color = color;
+    button.textColor = textColor;
+    button.clicked = false;
+    return button;
+}
+
+game GAME;
+
+void loadTexture() {
+    GAME.black_pawn_.black_pawn = LoadTexture("img/pedinanera-removebg-preview.png");
+    GAME.white_pawn_.white_pawn = LoadTexture("img/pedinabianca-removebg-preview.png");
+    GAME.board.black_place = LoadTexture("img/scacchiera_black.png");
+    GAME.board.white_place = LoadTexture("img/scacchiera_white.png");
+}
 
 void drawBoard() {
-    BOARDP.Black_space = 
-        LoadTexture("img/scacchiera_black.png");
-    BOARDP.White_space = 
-        LoadTexture("img/scacchiera_white.png");
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            if ((i + j) % 2 == 0) {
-                DrawTexture(BOARDP.White_space,
-                    j * SQUARE_SIZE, i * SQUARE_SIZE
-                    , WHITE
-                );
+    for (int row = 0; row < BOARD_SIZE; row++) {
+        for (int col = 0; col < BOARD_SIZE; col++) {
+            Vector2 pos = { col * SQUARE_SIZE, row * SQUARE_SIZE };
+
+            if ((row + col) % 2 == 0) {
+                DrawTextureEx(GAME.board.white_place, pos, 0.0f, 1.0f, WHITE);
             }
             else {
-                DrawTexture(BOARDP.Black_space,
-                    j * SQUARE_SIZE,
-                    i * SQUARE_SIZE,
-                    WHITE
-                );
+                DrawTextureEx(GAME.board.black_place, pos, 0.0f, 1.0f, WHITE);
+            }
+
+            if (GAME.white_pawn_.mat[row][col]) {
+                DrawTextureEx(GAME.white_pawn_.white_pawn, pos, 0.0f, 1.0f, WHITE);
+            }
+
+            if (GAME.black_pawn_.mat[row][col]) {
+                DrawTextureEx(GAME.black_pawn_.black_pawn, pos, 0.0f, 1.0f, WHITE);
             }
         }
     }
 }
 
-void drawPawn() {
-
-}
-
 int main() {
-    //iniziallizazione di tutte le varibili
     const int screenWidth = BOARD_SIZE * SQUARE_SIZE;
     const int screenHeight = BOARD_SIZE * SQUARE_SIZE;
-    Color buttonColor = BLUE;
-    Color textColor = WHITE;
-    Rectangle buttonRec1 = { screenWidth / 2 - 75, screenHeight / 2 - 25, 150, 50 };
-    Rectangle buttonRec2 = { screenWidth / 2 - 75, screenHeight / 2 + 50, 150, 50 };
-    const char* buttonText1 = "BOT";
-    const char* buttonText2 = "1 vs 1";
-    const char* MSG_WORK = "TRUE";
-    bool menu = true,
-         bot = false,
-         ovo = false;
-    InitWindow(screenWidth, screenHeight, "Dama");
-    //ToggleFullscreen();
 
-    while (!WindowShouldClose())
-    {
+    InitWindow(screenWidth, screenHeight, "Chess");
+
+    SetTargetFPS(60);
+
+    loadTexture();
+
+    Button vsBotButton = CreateButton(100,
+        100,
+        200, 
+        50, 
+        "vs Bot", 
+        20, 
+        GRAY, 
+        BLACK
+    );
+    Button oneVsOneButton = CreateButton(100, 
+        200, 
+        200, 
+        50, 
+        "1v1", 
+        20, 
+        GRAY, 
+        BLACK
+    );
+    GAME.Game_menu = true;
+    GAME.game_1v1 = false;
+    GAME.game_bot = false;
+    GAME.game_run = true;
+    GAME.game_over = false;
+
+    while (!WindowShouldClose()) {
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
-
-        if (menu == true) {
-            if (CheckCollisionPointRec(GetMousePosition(),
-                buttonRec1) &&
-                IsMouseButtonPressed(MOUSE_LEFT_BUTTON)
-                ) {
-                printf("%s\n", MSG_WORK);
-                menu = false;
-                bot = true;
+        if (GAME.Game_menu == true) {
+            DrawButton(vsBotButton);
+            DrawButton(oneVsOneButton);
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                Vector2 mousePos = GetMousePosition();
+                if (CheckCollisionPointRec(mousePos, vsBotButton.rect)) {
+                    drawBoard();
+                    GAME.game_bot = true;
+                    GAME.Game_menu = false;
+                }
+                if (CheckCollisionPointRec(mousePos, oneVsOneButton.rect)) {
+                    drawBoard();
+                    GAME.game_bot = true;
+                    GAME.Game_menu = false;
+                }
             }
-
-            if (CheckCollisionPointRec(GetMousePosition(),
-                buttonRec2) &&
-                IsMouseButtonPressed(MOUSE_LEFT_BUTTON)
-                ) {
-                printf("%s\n", MSG_WORK);
-                menu = false;
-                ovo = true;
-            }
-            DrawRectangleRec(buttonRec1, buttonColor);
-            DrawRectangleRec(buttonRec2, buttonColor);
-            DrawText(buttonText1, buttonRec1.x + buttonRec1.width / 2 - MeasureText(buttonText1, 20) / 2,
-                buttonRec1.y + buttonRec1.height / 2 - 10, 20, textColor);
-            DrawText(buttonText2, buttonRec2.x + buttonRec2.width / 2 - MeasureText(buttonText2, 20) / 2,
-                buttonRec2.y + buttonRec2.height / 2 - 10, 20, textColor);
         }
-        if (bot) {
-            drawBoard();
-        }
-        if (ovo) {
+        if (GAME.game_1v1 == true) {
             drawBoard();
         }
 
         EndDrawing();
-        UnloadTexture(BOARDP.Black_space);
-        UnloadTexture(BOARDP.White_space);
-
     }
+
     CloseWindow();
 
     return 0;
